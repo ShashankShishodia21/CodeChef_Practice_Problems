385a81a7b8932086481231fd492b87dc0d6b76db
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-25 16:28:16 +0530

    Create SQUATS.c

---
+++ SQUATS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&x);
+	    x=x*15;
+	    printf("%d\n",x);
+	}
+
+	return 0;
+}
+
