959a9e9f78f1e07738a74ea82b58e2690a093e1c
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-27 20:46:51 +0530

    Create LUDO.c

---
+++ LUDO.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&n);
+	    if(n==6){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
