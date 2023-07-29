8ff9af55ad9d29a0909d481628f6db66200ee1dd
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-29 21:06:28 +0530

    Create REACHTARGET.cpp

---
+++ REACHTARGET.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    printf("%d\n",a-b);
+	}
+	return 0;
+}
