c69fa5e629097874b8b2b3a99777f79646f7e2e9
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-31 17:13:22 +0530

    Create CNTWRD.c

---
+++ CNTWRD.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int m,n;
+	    scanf("%d %d",&m,&n);
+	    printf("%d\n",m*n);
+	}
+	return 0;
+}
+
