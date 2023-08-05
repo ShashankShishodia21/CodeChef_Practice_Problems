a21ce915177283b180458a5bf52f329465c4b919
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-08-05 23:30:42 +0530

    Create RAINFALL1.c

---
+++ RAINFALL1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+		int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x<3){
+	        printf("LIGHT\n");
+	    }
+	    else if(x>=3 && x<7){
+	        printf("MODERATE\n");
+	    }
+	    else if(x>=7){
+	        printf("HEAVY\n");
+	    }
+	}
+
+	return 0;
+}
+
