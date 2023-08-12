9b3841c2b9abbd839b6124503e44ce3feb1e2f00
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-08-12 23:18:42 +0530

    Create MARKSTW.c

---
+++ MARKSTW.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    if(x >= y*2){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	
+	return 0;
+}
+
