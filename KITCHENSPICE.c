0dcc4bece1fc7e55c2609a1c42e57ea269956563
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-08-21 21:38:16 +0530

    Create KITCHENSPICE.c

---
+++ KITCHENSPICE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	
+	    int x;
+	    scanf("%d",&x);
+	    if(x<4){
+	        printf("MILD\n");
+	    }
+	    else if(x>=4 && x<7){
+	        printf("MEDIUM\n");
+	    }
+	    else if(x>=7){
+	        printf("HOT\n");
+	    }
+	}
+	return 0;
+}
+
