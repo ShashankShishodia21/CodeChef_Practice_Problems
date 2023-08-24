4e71596865fb3c45053ed00060eafa6c3843af11
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-08-24 23:34:31 +0530

    Create HOTCOLD.c

---
+++ HOTCOLD.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x>20){
+	        printf("HOT\n");
+	    }
+	    else{
+	        printf("COLD\n");
+	    }
+	    
+	}
+	return 0;
+}
+
